18d3380d10d5553b13f82e2b8531ed2bb28fa59d
minamalaj <mina.malaj@hotmail.com>
2020-05-11 14:07:36 +0100

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,3 +9,13 @@ class Application
     end 
 
 end
+
+# class Application 
+#     def call(env)
+#         resp = Rack::Response.new
+
+#         Time.now.hour < 12 ? resp.write "Good Morning" ? resp.write "Good Afternoon"
+
+#         resp.finish
+#     end 
+# end 
