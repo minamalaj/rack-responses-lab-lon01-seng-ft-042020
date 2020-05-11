f55715c438b0dfa89fa0a620ffd0b0897b2ed6d0
minamalaj <mina.malaj@hotmail.com>
2020-05-11 14:04:53 +0100

    Done.

---
+++ app/application.rb
@@ -0,0 +1,11 @@
+class Application
+    
+    def call(env)
+        return [200, {'Content-Type' => 'text/html'}, pretty_response]
+    end 
+    
+    def pretty_response
+        Time.now.hour < 12 ? ["<em>Good Morning</em>"] : ["<em>Good Afternoon</em>"]
+    end 
+
+end
